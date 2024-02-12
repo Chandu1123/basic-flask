1112b5429d72b97b7be7b86bf59f8991d2cc6f8b
Chandu1123 <chandusarithala555@gmail.com>
2024-02-12 23:53:51 +0530

    codes updated

--- app.py
+++ app.py
@@ -1,36 +1,22 @@
 ## basic flask app here...
 
 
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/members')
-# def welcome():
-#     return "welcome chandu hi raa"
-
-# @app.route('/')
-# def welcome1():
-#     return "dfghjkiuytf dfghjuytrdxcvbjuytrdxcvbniuytrdxbkuytrdxcvbjuytrdxcvbjuytrxd"
-
-
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-# Building URL dynamically
-    
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/members')
+def welcome():
+    return "welcome chandu hi raa"
+
 @app.route('/')
-def chandu():
-    return "ccccccccchhhhhhhhhhaaaaaaaaaaannnnnnnnnnnduuuuuu"
+def welcome1():
+    return "dfghjkiuytf dfghjuytrdxcvbjuytrdxcvbniuytrdxbkuytrdxcvbjuytrdxcvbjuytrxd"
 
 
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+
